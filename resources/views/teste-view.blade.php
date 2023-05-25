0618f54f17bf0b0fafb22bb81bc13dbc90c898da
anagiu <126169431+anagiu@users.noreply.github.com>
2023-05-25 08:56:00 -0300

    Initial commit

---
+++ resources/views/teste-view.blade.php
@@ -0,0 +1,9 @@
+@extends('includes.base')
+
+@section('title', 'Título')
+
+@section('content')
+<p> Este é meu primeiro arquivo Blade</p>
+<p> ( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°) </p>
+<p> minha variável que veio de longe: {{ $valor_da_controller }}</p>
+@endsection
