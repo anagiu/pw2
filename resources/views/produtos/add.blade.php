85a8d1bd5b2509ed44ca1f974620403d33954524
anagiu <126169431+anagiu@users.noreply.github.com>
2023-05-26 11:00:37 -0300

    produtos

---
+++ resources/views/produtos/add.blade.php
@@ -0,0 +1,21 @@
+@extends('includes.base')
+
+@section('title', 'Produtos - Adicionar')
+
+@section('content')
+    <h2>Adicione seu Produto</h2>
+
+    <form action="{{ route('produtos.addSave')}}" method="post">
+        @csrf
+        <input type="text" name="name"
+        placeholder="Nome do produto">
+        <br>
+        <input type="number" name="price" step="0.01"
+        placeholder="Preço">
+        <br>
+        <input type="number" name="quantity"
+        placeholder="Quantidade" min='0'>
+        <hr width="30%" align="left">
+        <input type="submit" value="Gravar">
+    </form>
+@endsection
--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -1,2 +1,8 @@
-@extends('includes.base');
+@extends('includes.base')
 
+@section('title', 'produtos')
+
+@section('content')
+    <p>Produtos Funcionando</p>
+    <a href="{{ route('produtos.add')}}">Adicionar Produto</a>
+@endsection
