3cc413e1274bc8c99f07d6c281f040d04d17ae8b
anagiu <126169431+anagiu@users.noreply.github.com>
2023-06-02 10:46:51 -0300

    tradução e view

--- resources/views/produtos/add.blade.php
+++ resources/views/produtos/add.blade.php
@@ -15,13 +15,13 @@
     <form action="{{ route('produtos.addSave')}}" method="post">
         @csrf
         <input type="text" name="name"
-        placeholder="Nome do produto">
+        placeholder="Nome do produto" value="{{ old ('name')}}">
         <br>
         <input type="number" name="price" step="0.01"
-        placeholder="Preço">
+        placeholder="Preço" value="{{ old ('price')}}">
         <br>
         <input type="number" name="quantity"
-        placeholder="Quantidade" min='0'>
+        placeholder="Quantidade" min='0' value="{{ old ('quantity')}}">
         <hr width="30%" align="left">
         <input type="submit" value="Gravar">
     </form>
